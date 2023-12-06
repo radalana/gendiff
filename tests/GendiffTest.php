dd2fa247c80a16989d686a19e2c844d009714a5d
Svetlana <radalana415@gmail.com>
2023-12-06 16:32:58 +0100

    Reformat GendiffTest.php

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -10,11 +10,13 @@ class GendiffTest extends TestCase
 {
     public function testGendiff(): void
     {
-        $filePath1 = './tests/fixtures/file1.json';//tests/fixtures/file2.json
+        $filePath1 = './tests/fixtures/file1.json';
         $filePath2 = './tests/fixtures/file2.json';
         $expected = "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}\n";
         
         $this->assertEquals($expected, gendiff($filePath1, $filePath2));  
 
     }
+
+
 }
