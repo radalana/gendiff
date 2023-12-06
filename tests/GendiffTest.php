263c835688b256a5b7be3be67132db7a862386be
Svetlana <radalana415@gmail.com>
2023-12-06 14:41:44 +0100

    Add GendiffTest.php

---
+++ tests/GendiffTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Code\Phpunit\Tests;
+
+use PHPUnit\Framework\TestCase;
+use SebastianBergmann\Type\VoidType;
+use function Code\Gendiff\gendiff;
+
+class GendiffTest extends TestCase
+{
+    public function testGendiff(): void
+    {
+        $filePath1 = 'fixtures/file1.json';
+        $filePath2 = 'fixtures/file2.json';
+
+        $expected = "{\n
+            - follow: false\n
+              host: hexlet.io\n
+            - proxy: 123.234.53.22\n
+            - timeout: 50\n
+            + timeout: 20\n
+            + verbose: true\n
+          }\n"
+        
+        $this->assertEquals($expected, gendiff($filePath1, $filePath2));  
+
+    }
+}
