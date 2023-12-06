f061ed9bdd27feb3589b3854c7e8031222906ba6
Svetlana <radalana415@gmail.com>
2023-12-06 15:42:12 +0100

    Refactor tests/GendiffTest.php
    fix $expected in testGendiff function

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -3,24 +3,16 @@
 namespace Code\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
-use SebastianBergmann\Type\VoidType;
+
 use function Code\Gendiff\gendiff;
 
 class GendiffTest extends TestCase
 {
     public function testGendiff(): void
     {
-        $filePath1 = 'fixtures/file1.json';
-        $filePath2 = 'fixtures/file2.json';
-
-        $expected = "{\n
-            - follow: false\n
-              host: hexlet.io\n
-            - proxy: 123.234.53.22\n
-            - timeout: 50\n
-            + timeout: 20\n
-            + verbose: true\n
-          }\n"
+        $filePath1 = 'file1.json';//tests/fixtures/file2.json
+        $filePath2 = 'file2.json';
+        $expected = "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}\n";
         
         $this->assertEquals($expected, gendiff($filePath1, $filePath2));  
 
