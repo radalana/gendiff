0b8a054b57827c4ba2106ab3b9569ab80c542288
Svetlana <radalana415@gmail.com>
2023-12-13 14:24:57 +0100

    Test add yml tests

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -10,44 +10,74 @@ use function Code\Gendiff\gendiff;
 class GendiffTest extends TestCase {
     public function testGendiff(): void
     {
-        $filePath1 = './tests/fixtures/test1/file1.json';
-        $filePath2 = './tests/fixtures/test1/file2.json';
         $expected = "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}\n";
-        $this->assertEquals($expected, gendiff($filePath1, $filePath2));
+
+        $jsonFile1 = './tests/fixtures/jsonFiles/test1/file1.json';
+        $jsonFile2 = './tests/fixtures/jsonFiles/test1/file2.json';
+        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
+
+        $ymlFile1 = './tests/fixtures/ymlFiles/test1/file1.yml';
+        $ymlFile2 = './tests/fixtures/ymlFiles/test1/file2.yml';
+        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
     }
 
     public function testGendiff2(): void
     {
-        $filePath1 = './tests/fixtures/test2/file1.json';
-        $filePath2 = './tests/fixtures/test2/file2.json';
         $expected = "{\n+ api-key: 550e\n+ api-version: v1\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}\n";
-        $this->assertEquals($expected, gendiff($filePath1, $filePath2));
+
+        $jsonFile1 = './tests/fixtures/jsonFiles/test2/file1.json';
+        $jsonFile2 = './tests/fixtures/jsonFiles/test2/file2.json';
+        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
+        
+        $ymlFile1 = './tests/fixtures/ymlFiles/test2/file1.yml';
+        $ymlFile2 = './tests/fixtures/ymlFiles/test2/file2.yml';
+        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
+        
     }
 
     //**same keys, all values changed*/
     public function testGendiff3(): void
     {
-        $filePath1 = './tests/fixtures/test3/file1.json';
-        $filePath2 = './tests/fixtures/test3/file2.json';
         $expected = "{\n- api-key: 550e\n+ api-key: 770k\n- api-version: v1\n+ api-version: v2\n- host: hexlet.io\n+ host: www.example.com\n- timeout: 20\n+ timeout: 50\n- verbose: true\n+ verbose: false\n}\n";
-        $this->assertEquals($expected, gendiff($filePath1, $filePath2));
+
+        $jsonFile1 = './tests/fixtures/jsonFiles/test3/file1.json';
+        $jsonFile2 = './tests/fixtures/jsonFiles/test3/file2.json';
+        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
+
+        $ymlFile1 = './tests/fixtures/ymlFiles/test3/file1.yml';
+        $ymlFile2 = './tests/fixtures/ymlFiles/test3/file2.yml';
+        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
+
+        
     }
 
     /** First file is empty*/
     public function testEmptyFile(): void
     {
-        $filePath1 = './tests/fixtures/test4/file1.json';
-        $filePath2 = './tests/fixtures/test4/file2.json';
-        $this->expectExceptionMessage("{$filePath1} is empty!");
-        gendiff($filePath1, $filePath2);
+        $jsonFile1 = './tests/fixtures/jsonFiles/test4/file1.json';
+        $jsonFile2 = './tests/fixtures/jsonFiles/test4/file2.json';
+        $this->expectExceptionMessage("{$jsonFile1} is empty!");
+        gendiff($jsonFile1, $jsonFile2);
+
+        $ymlFile1 = './tests/fixtures/ymlFiles/test4/file1.yml';
+        $ymlFile2 = './tests/fixtures/ymlFiles/test4/file2.yml';
+        $this->expectExceptionMessage("{$ymlFile1} is empty!");
+        gendiff($ymlFile1, $ymlFile2);
+        
     }
 
     public function testIdenticalFiles(): void
     {
-        $filePath1 = './tests/fixtures/test5/file1.json';
-        $filePath2 = './tests/fixtures/test5/file2.json';
         $expected = "{\n  host: hexlet.io\n  timeout: 20\n  verbose: true\n}\n";
-        $this->assertEquals($expected, gendiff($filePath1, $filePath2)); 
-    }
-}
 
+        $jsonFile1 = './tests/fixtures/jsonFiles/test5/file1.json';
+        $jsonFile2 = './tests/fixtures/jsonFiles/test5/file2.json';
+        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
+
+        $ymlFile1 = './tests/fixtures/ymlFiles/test5/file1.yml';
+        $ymlFile2 = './tests/fixtures/ymlFiles/test5/file2.yml';
+        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
+
+    }
+    
+}
