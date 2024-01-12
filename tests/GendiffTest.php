cd814ae4a2cd5953fe79b944ca4794a2a0962626
Svetlana <radalana415@gmail.com>
2024-01-12 20:43:55 +0100

    Test 5
    test from first file deleted list

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -85,6 +85,14 @@ class GendiffTest extends TestCase {
         $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
     }
 
+    public function testGendiffJson5()
+    {
+        $jsonFile1 = './tests/fixtures/jsonFiles/test5/file1.json';
+        $jsonFile2 = './tests/fixtures/jsonFiles/test5/file2.json';
+        $expected = file_get_contents('./tests/fixtures/jsonFiles/test5/expected.json');
+        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
+    }
+
     public function testGendiffYml()
     {
         $ymlFile1 = './tests/fixtures/ymlFiles/file1.yml';
