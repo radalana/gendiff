20e98d6882b9a2731d1996c71b3dac8f118313df
Svetlana <radalana415@gmail.com>
2024-01-17 16:50:56 +0100

    Change file names according to namespaces

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\gendiff;
 
-class GendiffTest extends TestCase
+class DifferTest extends TestCase
 {
     public function testGendiffJson1()
     {
