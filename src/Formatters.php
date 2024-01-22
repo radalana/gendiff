712a704e433d951a9883f8a2c60813bef921dc67
Svetlana <radalana415@gmail.com>
2024-01-22 13:25:31 +0100

    Refactor src/Formatters.php acc. phpstan8

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,6 +6,11 @@ use function Differ\Formatters\Plain\toPlain;
 use function Differ\Formatters\Stylish\style;
 use function Differ\Formatters\Json\toJson;
 
+/**
+ * @param string $format
+ * @param array<string, mixed> $ast
+ * @return string
+ */
 function format(string $format, array $ast): string
 {
     if ($format === 'plain') {
