16e4c60885ec0d3804818a5cb2daeee4513e5d55
Svetlana <radalana415@gmail.com>
2024-01-24 17:52:54 +0100

    Update Tests
    update file tests/DifferTest.php: add data providers

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,186 +8,76 @@ use function Differ\Differ\gendiff;
 
 class DifferTest extends TestCase
 {
-    public function testGendiffJson1(): void
+
+    /**
+     * @dataProvider gendiffStylishProvider
+     */
+    public function testGendiffStylish(string $path1, string $path2, string $expectedFile): void
     {
-        $jsonFile1 = './tests/fixtures/test1/json/file1.json';
-        $jsonFile2 = './tests/fixtures/test1/json/file2.json';
-        //stylish
-        $actualDefaultStylish = trim(gendiff($jsonFile1, $jsonFile2));
-        $expected =  trim(file_get_contents('./tests/fixtures/test1/expected'));
-        $this->assertEquals($expected, $actualDefaultStylish);
+        $result = trim((gendiff($path1, $path2, 'stylish')));
+        $expected =  trim(file_get_contents($expectedFile));
+        $this->assertEquals($expected, $result);
 
-        $actual = trim(gendiff($jsonFile1, $jsonFile2, 'stylish'));
-        $this->assertEquals($expected, $actual);
-
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test1/expectedPlain.txt'));
-        $this->assertEquals($expectedPlain, trim(gendiff($jsonFile1, $jsonFile2, 'plain')));
-
-        //json
-        $expectedJsonFile = './tests/fixtures/test1/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($jsonFile1, $jsonFile2, 'json'));
+        $resultDefault = trim((gendiff($path1, $path2)));
+        $this->assertEquals($expected, $resultDefault);
+        
+    }
+    public static function gendiffStylishProvider(){
+        return [['./tests/fixtures/test1/json/file1.json', './tests/fixtures/test1/json/file2.json', './tests/fixtures/test1/expected.json'],
+                ['./tests/fixtures/test1/yml/file1.yml', './tests/fixtures/test1/yml/file2.yml', './tests/fixtures/test1/expected.json'],
+                ['./tests/fixtures/test2/json/file1.json', './tests/fixtures/test2/json/file2.json', './tests/fixtures/test2/expected.json'],
+                ['./tests/fixtures/test2/yml/file1.yml', './tests/fixtures/test2/yml/file2.yml', './tests/fixtures/test2/expected.json'],
+                ['./tests/fixtures/test3/json/file1.json', './tests/fixtures/test3/json/file2.json', './tests/fixtures/test3/expected.json'],
+                ['./tests/fixtures/test3/yml/file1.yml', './tests/fixtures/test3/yml/file2.yml', './tests/fixtures/test3/expected.json'],
+                ['./tests/fixtures/test4/json/file1.json', './tests/fixtures/test4/json/file2.json', './tests/fixtures/test4/expected.json'],
+                ['./tests/fixtures/test4/yml/file1.yml', './tests/fixtures/test4/yml/file2.yml', './tests/fixtures/test4/expected.json'],
+                ['./tests/fixtures/test5/json/file1.json', './tests/fixtures/test5/json/file2.json', './tests/fixtures/test5/expected.json'],
+                ['./tests/fixtures/test5/yml/file1.yml', './tests/fixtures/test5/yml/file2.yml', './tests/fixtures/test5/expected.json']
+    ];
     }
 
-    public function testGendiffYml1(): void
+    /**
+     * @dataProvider gendiffPlainProvider
+     */
+    public function testGendiffPlain($path1, $path2, $expectedPath)
     {
-        $ymlFile1 = './tests/fixtures/test1/yml/file1.yml';
-        $ymlFile2 = './tests/fixtures/test1/yml/file2.yml';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test1/expected'));
-        $this->assertEquals($expected, trim(gendiff($ymlFile1, $ymlFile2)));
-        $this->assertEquals($expected, trim(gendiff($ymlFile1, $ymlFile2, 'stylish')));
-
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test1/expectedPlain.txt'));
-        $this->assertEquals($expectedPlain, trim(gendiff($ymlFile1, $ymlFile2, 'plain')));
-
-        //json
-        $expectedJsonFile = './tests/fixtures/test1/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($ymlFile1, $ymlFile2, 'json'));
+        $expected = trim(file_get_contents($expectedPath));
+        $this->assertEquals($expected, trim(gendiff($path1, $path2, 'plain')));
+    }
+    public static function gendiffPlainProvider()
+    {
+        return [['./tests/fixtures/test1/json/file1.json', './tests/fixtures/test1/json/file2.json', './tests/fixtures/test1/expectedPlain'],
+                ['./tests/fixtures/test1/yml/file1.yml', './tests/fixtures/test1/yml/file2.yml', './tests/fixtures/test1/expectedPlain'],
+                ['./tests/fixtures/test2/json/file1.json', './tests/fixtures/test2/json/file2.json', './tests/fixtures/test2/expectedPlain'],
+                ['./tests/fixtures/test2/yml/file1.yml', './tests/fixtures/test2/yml/file2.yml', './tests/fixtures/test2/expectedPlain'],
+                ['./tests/fixtures/test3/json/file1.json', './tests/fixtures/test3/json/file2.json', './tests/fixtures/test3/expectedPlain'],
+                ['./tests/fixtures/test3/yml/file1.yml', './tests/fixtures/test3/yml/file2.yml', './tests/fixtures/test3/expectedPlain'],
+                ['./tests/fixtures/test4/json/file1.json', './tests/fixtures/test4/json/file2.json', './tests/fixtures/test4/expectedPlain'],
+                ['./tests/fixtures/test4/yml/file1.yml', './tests/fixtures/test4/yml/file2.yml', './tests/fixtures/test4/expectedPlain'],
+                ['./tests/fixtures/test5/json/file1.json', './tests/fixtures/test5/json/file2.json', './tests/fixtures/test5/expectedPlain'],
+                ['./tests/fixtures/test5/yml/file1.yml', './tests/fixtures/test5/yml/file2.yml','./tests/fixtures/test5/expectedPlain']
+    ];
     }
 
-    /**Test data with list as a value */
-    public function testGendiffJson2(): void
+    /**
+     * @dataProvider gendiffJsonProvider
+     */
+    public function testGendiffJson($path1, $path2, $expectedPath)
     {
-        $jsonFile1 = './tests/fixtures/test2/json/file1.json';
-        $jsonFile2 = './tests/fixtures/test2/json/file2.json';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test2/expected.json'));
-        $this->assertEquals($expected, trim(gendiff($jsonFile1, $jsonFile2)));
-        $this->assertEquals($expected, trim(gendiff($jsonFile1, $jsonFile2, 'stylish')));
-
-         //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test2/expectedPlain'));
-        $this->assertEquals($expectedPlain, trim(gendiff($jsonFile1, $jsonFile2, 'plain')));
-        //json
-        $expectedJsonFile = './tests/fixtures/test2/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($jsonFile1, $jsonFile2, 'json'));
+        $this->assertJsonStringEqualsJsonFile($expectedPath, trim(gendiff($path1, $path2, 'json')));
     }
-
-    public function testGendiffYml2(): void
+    public static function gendiffJsonProvider()
     {
-        $ymlFile1 = './tests/fixtures/test2/yml/file1.yml';
-        $ymlFile2 = './tests/fixtures/test2/yml/file2.yml';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test2/expected.json'));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2, 'stylish'));
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test2/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($ymlFile1, $ymlFile2, 'plain'));
-        //json
-        $expectedJsonFile = './tests/fixtures/test2/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($ymlFile1, $ymlFile2, 'json'));
-    }
-
-    public function testGendiffJson3(): void
-    {
-        $jsonFile1 = './tests/fixtures/test3/json/file1.json';
-        $jsonFile2 = './tests/fixtures/test3/json/file2.json';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test3/expected.json'));
-        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
-        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2, 'stylish'));
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test3/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($jsonFile1, $jsonFile2, 'plain'));
-        //json
-        $expectedJsonFile = './tests/fixtures/test3/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($jsonFile1, $jsonFile2, 'json'));
-    }
-
-    public function testGendiffYml3(): void
-    {
-        $ymlFile1 = './tests/fixtures/test3/yml/file1.yml';
-        $ymlFile2 = './tests/fixtures/test3/yml/file2.yml';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test3/expected.json'));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2, 'stylish'));
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test3/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($ymlFile1, $ymlFile2, 'plain'));
-        //json
-        $expectedJsonFile = './tests/fixtures/test3/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($ymlFile1, $ymlFile2, 'json'));
-    }
-
-    /** Test data with a list as a part of complex value */
-    public function testGendiffJson4(): void
-    {
-        $jsonFile1 = './tests/fixtures/test4/json/file1.json';
-        $jsonFile2 = './tests/fixtures/test4/json/file2.json';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test4/expected.json'));
-        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
-        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2), 'stylish');
-
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test4/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($jsonFile1, $jsonFile2, 'plain'));
-
-        //json
-        $expectedJsonFile = './tests/fixtures/test4/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($jsonFile1, $jsonFile2, 'json'));
-    }
-
-    public function testGendiffYml4(): void
-    {
-        $ymlFile1 = './tests/fixtures/test4/yml/file1.yml';
-        $ymlFile2 = './tests/fixtures/test4/yml/file2.yml';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test4/expected.json'));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2, 'stylish'));
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test4/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($ymlFile1, $ymlFile2, 'plain'));
-        //json
-        $expectedJsonFile = './tests/fixtures/test4/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($ymlFile1, $ymlFile2, 'json'));
-    }
-
-    public function testGendiffJson5(): void
-    {
-        $jsonFile1 = './tests/fixtures/test5/json/file1.json';
-        $jsonFile2 = './tests/fixtures/test5/json/file2.json';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test5/expected.json'));
-        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2));
-        $this->assertEquals($expected, gendiff($jsonFile1, $jsonFile2), 'stylish');
-
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test5/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($jsonFile1, $jsonFile2, 'plain'));
-
-        //json
-        $expectedJsonFile = './tests/fixtures/test5/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($jsonFile1, $jsonFile2, 'json'));
-    }
-
-    public function testGendiffYml5(): void
-    {
-        $ymlFile1 = './tests/fixtures/test5/yml/file1.yml';
-        $ymlFile2 = './tests/fixtures/test5/yml/file2.yml';
-
-        //stylish
-        $expected = trim(file_get_contents('./tests/fixtures/test5/expected.json'));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2));
-        $this->assertEquals($expected, gendiff($ymlFile1, $ymlFile2, 'stylish'));
-        //plain text
-        $expectedPlain = trim(file_get_contents('./tests/fixtures/test5/expectedPlain'));
-        $this->assertEquals($expectedPlain, gendiff($ymlFile1, $ymlFile2, 'plain'));
-        //json
-        $expectedJsonFile = './tests/fixtures/test5/expectedJson.json';
-        $this->assertJsonStringEqualsJsonFile($expectedJsonFile, gendiff($ymlFile1, $ymlFile2, 'json'));
+        return [['./tests/fixtures/test1/json/file1.json', './tests/fixtures/test1/json/file2.json', './tests/fixtures/test1/expectedJson.json'],
+                ['./tests/fixtures/test1/yml/file1.yml', './tests/fixtures/test1/yml/file2.yml', './tests/fixtures/test1/expectedJson.json'],
+                ['./tests/fixtures/test2/json/file1.json', './tests/fixtures/test2/json/file2.json', './tests/fixtures/test2/expectedJson.json'],
+                ['./tests/fixtures/test2/yml/file1.yml', './tests/fixtures/test2/yml/file2.yml', './tests/fixtures/test2/expectedJson.json'],
+                ['./tests/fixtures/test3/json/file1.json', './tests/fixtures/test3/json/file2.json', './tests/fixtures/test3/expectedJson.json'],
+                ['./tests/fixtures/test3/yml/file1.yml', './tests/fixtures/test3/yml/file2.yml', './tests/fixtures/test3/expectedJson.json'],
+                ['./tests/fixtures/test4/json/file1.json', './tests/fixtures/test4/json/file2.json', './tests/fixtures/test4/expectedJson.json'],
+                ['./tests/fixtures/test4/yml/file1.yml', './tests/fixtures/test4/yml/file2.yml', './tests/fixtures/test4/expectedJson.json'],
+                ['./tests/fixtures/test5/json/file1.json', './tests/fixtures/test5/json/file2.json', './tests/fixtures/test5/expectedJson.json'],
+                ['./tests/fixtures/test5/yml/file1.yml', './tests/fixtures/test5/yml/file2.yml','./tests/fixtures/test5/expectedJson.json']
+    ];
     }
 }
