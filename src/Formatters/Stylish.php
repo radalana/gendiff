3bfa248eb8aa2b15a5ee5cff63b86b9c04f27016
Svetlana <radalana415@gmail.com>
2024-03-07 18:55:13 +0100

    Update Stylish.php
    результирующая строка строится теперь сразу из дерева, без промежуточного вложенного массива со знаками
    
    P.s еще не реализована обработка списков

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -33,7 +33,7 @@ function objectToArray(mixed $data): mixed
  * @param array<int|string, mixed> $diff
  * @return array<mixed>
  */
-function addSign(array $diff): array
+function addSign1(array $diff): array
 {
     $iter = function ($data) use (&$iter) {
         $differ = $data['differ'];
@@ -43,6 +43,7 @@ function addSign(array $diff): array
                 $newChildren = array_merge(...array_map(fn($child) => $iter($child), $children));
                 return [$data['key'] => $newChildren];
             case 'added':
+                //var_dump('это условие не должно сработать, что за пиздец');
                 return ["+ {$data['key']}" => objectToArray($data['value'])];
             case 'deleted':
                 return ["- {$data['key']}" => objectToArray($data['value'])];
@@ -59,28 +60,35 @@ function addSign(array $diff): array
     return [array_merge(...(array_map(fn($data) => $iter($data), $diff)))];
 }
 
+
 /**
- * @param array<string, mixed> $data
+ * @param array<int|string, mixed> $ast
  * @return string
  */
-function stringify(array $data): string
+function format1(array $ast): string
 {
-    print_r($data);
+    $arrayWithSigns = addSign($ast);
+    return stringify(array_merge(...$arrayWithSigns));
+}
+function stringify2(array $data): string
+{
+    
     {
-        $iter = function (mixed $currentValue, int $depth) use (&$iter) {
-            if (!is_array($currentValue)) {
-                return toString($currentValue);
+        $iter = function (mixed $currentData, int $depth) use (&$iter) {
+            if (!is_array($currentData)) {
+                return toString($currentData);
             }
 
             $indentSize = $depth * SPACES_COUNT;
             $currentIndent = str_repeat(REPLACER, $indentSize);
             $signIndent = str_repeat(REPLACER, ($indentSize - 2));
             $bracketIndent = str_repeat(REPLACER, $indentSize - SPACES_COUNT);
-            if (isIndexedArray($currentValue)) { //для обвчного массива
-                $string = implode(', ', $currentValue);
+            if (isIndexedArray($currentData)) { //для обвчного массива
+                $string = implode(', ', $currentData);
                 return "[{$string}]";
             }
             $lines = array_map(function ($key, $val) use ($currentIndent, $signIndent, &$iter, &$depth) {
+                ////var_dump($key);
                 if (is_string($key) && ($key[0] === '+' || $key[0] === '-')) {
                     return "{$signIndent}{$key}: {$iter($val, $depth+1)}";
                 }
@@ -88,7 +96,7 @@ function stringify(array $data): string
                     return "{$currentIndent}{$iter($val, $depth+1)}";
                 }
                 return "{$currentIndent}{$key}: {$iter($val, $depth+1)}";
-            }, array_keys($currentValue), $currentValue);
+            }, array_keys($currentData), $currentData);
                      $result = ['{', ...$lines, "{$bracketIndent}}"];
 
             return implode("\n", $result);
@@ -96,27 +104,100 @@ function stringify(array $data): string
         return $iter($data, 1);
     }
 }
-/**
- * @param array<int|string, mixed> $ast
- * @return string
- */
-function format(array $ast): string
-{
-    $arrayWithSigns = addSign($ast);
-    return stringify(array_merge(...$arrayWithSigns));
-}
+function stringify1($data, $depth) {
+    if (!is_array($data)) {
+        return toString($data);
+    }
+    $indentSize = $depth * SPACES_COUNT;
+    $currentIndent = str_repeat(REPLACER, $indentSize);
+    $bracketIndent = str_repeat(REPLACER, $indentSize - SPACES_COUNT);
+    $lines = array_map(function($key, $value) use ($currentIndent, &$depth) {
+        $value = stringify1($value, $depth + 1);
+        return "{$currentIndent}{$key}: {$value}";
 
-function format1(array $ast)
+    }, array_keys($data), $data);
+    $result = ['{', ...$lines, "{$bracketIndent}}"];
+    return implode("\n", $result);
+
+}
+function stringify(array $data)
 {
-    print_r($ast);
-    $iter = function ($currentValue, int $depth) use (&$iter) {
-        $indentSize = $depth * SPACES_COUNT;
+    //var_dump($data);
+    
+        $iter = function (mixed $currentData, int $depth) use (&$iter) {
+            if (!is_array($currentData)) {
+                return toString($currentData);
+            }
+            
+            $indentSize = $depth * SPACES_COUNT;
             $currentIndent = str_repeat(REPLACER, $indentSize);
             $signIndent = str_repeat(REPLACER, ($indentSize - 2));
-            $bracketIndent = str_repeat(REPLACER, $indentSize - SPACES_COUNT);
+            
+            //var_dump('изначально', $currentData);
+            $differ = $currentData['differ'];
+            switch($differ) {
+                case 'added':
+                     $valAsArray = objectToArray($currentData['value']);
+                     //var_dump($valAsArray);
+                     $stringified = stringify1($valAsArray, $depth+1);
+                     //var_dump($stringified);
+                     $added = "{$signIndent}+ {$currentData['key']}: {$stringified}";
+                     //print_r("###############################");
+                     //var_dump($added);
+                     return $added;
+                     
+                
+                case 'deleted':
+                    $valAsArray = objectToArray($currentData['value']);
+                    $stringified = stringify1($valAsArray, $depth+1);
+                    $deleted = "{$signIndent}- {$currentData['key']}: {$stringified}";
+                    return $deleted;
+                
+                case 'unchanged':
+                    $valAsArray = objectToArray($currentData['value']);
+                    $stringified = stringify1($valAsArray, $depth);
+  
+                    $unchanged = "{$currentIndent}{$currentData['key']}: {$stringified}";
+                    return $unchanged;
+                
+                case 'changed':
+                    $val1 = stringify1(objectToArray($currentData['value1']), $depth+1);
+                    $val2 = stringify1(objectToArray($currentData['value2']), $depth+1);
+                    $changed = "{$signIndent}- {$currentData['key']}: {$val1}\n{$signIndent}+ {$currentData['key']}: {$val2}";
+                    return $changed;
+                case 'nested':
+                    ////var_dump($currentData);
+                    $children = getChildren($currentData);
+                    ////var_dump($children);
+                    $nested = array_map(fn($child) => $iter($child, $depth+1), $children);
+                    //var_dump($nested);
+                    $linesNested =  ['{', ...$nested, "{$currentIndent}}"];
+                    //var_dump($linesNested);
+                    $stringNested =  implode("\n", $linesNested);
+                    //var_dump($stringNested);
+                    $result = "{$currentIndent}{$currentData['key']}: {$stringNested}";
+                    return $result;
+                default:
+                    throw new \Exception('Not valid differ of node!');
+                
+            }
+        };
+        
+        $result =  implode("\n", array_map(function($node) use($iter){
+            //var_dump($node);
+            return $iter($node, 1);
+        }, $data));
+        return "{\n{$result}\n}";
+        
+    
+}
 
-            $lines = array_map(function ($key, $value) use ($currentIndent, $signIndent, &$iter, &$depth) {
-            }, array_keys($currentValue), $currentValue);
-    };
-    return '';
+function format($ast)
+{
+    $r = stringify($ast);
+    ////var_dump($r);
+    //print_r("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+    //print_r($r);
+    #print_r($r);
+    return $r;
 }
